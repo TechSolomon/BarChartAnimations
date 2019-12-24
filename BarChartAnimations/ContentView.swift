2d8f60c8d6d368b6b952013f6c438c04eb121fbf
Solomon Himelbloom <2001solomon@gmail.com>
2019-12-24 00:09:05 -0900

    Added colored bar charts above day text.

--- BarChartAnimations/ContentView.swift
+++ BarChartAnimations/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // A default pickerSelectedItem Value yields the following:
+    // 0 = Weekday, 1 = Afternoon, and 2 = Evening.
     @State var pickerSelectedItem = 0
     
     var body: some View {
@@ -23,10 +25,23 @@ struct ContentView: View {
                     .fontWeight(.heavy)
                 
                 Picker(selection: $pickerSelectedItem, label: Text("")) {
-                    Text("Weekday")
-                    Text("Afternoon")
-                    Text("Evening")
+                    Text("Weekday").tag(0)
+                    Text("Afternoon").tag(1)
+                    Text("Evening").tag(2)
                 }.pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal, 24)
+                
+                HStack {
+                    VStack {
+                        ZStack (alignment: .bottom) {
+                            Capsule().frame(width: 30, height: 200)
+                                .foregroundColor(Color(#colorLiteral(red: 0.2509803922, green: 0.8862745098, blue: 1, alpha: 1)))
+                            Capsule().frame(width: 30, height: 100)
+                                .foregroundColor(.white)
+                        }
+                        Text("D").padding(.top, 8)
+                    }
+                }.padding(.top, 24)
             }
             
         }
