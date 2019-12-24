dd4e5c0ace46e6785bb0acbfb87b1bb5dab32bb7
Solomon Himelbloom <2001solomon@gmail.com>
2019-12-23 22:42:01 -0900

    Initial Commit

---
+++ BarChartAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartAnimations
+//
+//  Created by Solomon on 12/23/19.
+//  Copyright © 2019 TechSolomon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
